8a8a1bac818a8b57e06f5b3a32dcbfa300b525d3
Xinyue Zhang (RainyDreams) <cfzxycn@gmail.com>
2025-02-18 21:55:04 +0800

    feat(app): 添加 WebView 并加载指定网址
    - 在 MainActivity 中添加 WebView 组件
    - 配置 WebView 设置，启用 JavaScript
    - 设置 WebViewClient 以在 WebView 中打开链接
    - 加载指定网址：https://lb.chiziingiin.top

--- app/src/main/java/com/chiziingiin/originsynq/MainActivity.java
+++ app/src/main/java/com/chiziingiin/originsynq/MainActivity.java
@@ -1,5 +1,29 @@
 package com.chiziingiin.originsynq;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 获取WebView实例
+        webView = findViewById(R.id.webView);
+
+        // 配置WebView
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true); // 启用JavaScript
+        webView.setWebViewClient(new WebViewClient()); // 确保在WebView中打开链接
+
+        // 加载网址
+        webView.loadUrl("https://lb.chiziingiin.top");
+    }
+}
